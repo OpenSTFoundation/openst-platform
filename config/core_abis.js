fe55bc84fa6b0602e495746949ba3b9aa190c92e
Puneet Khushwani for Pepo <puneet.khushwani@pepo.com>
2017-11-22 18:36:44 +0530

    utility token file

--- config/core_abis.js
+++ config/core_abis.js
@@ -1,4 +1,5 @@
-const core_abis = {
+const fs = require('fs'),
+  core_abis = {
   simpleToken: JSON.parse(fs.readFileSync('./contracts/abi/SimpleToken.abi', "utf8")),
   staking: JSON.parse(fs.readFileSync('./contracts/abi/Staking.abi', "utf8")),
   utilityToken: JSON.parse(fs.readFileSync('./contracts/abi/UtilityToken.abi', "utf8"))
