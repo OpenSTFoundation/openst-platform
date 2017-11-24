c15fb063cab688b48124800e7fd9a42c2d6c5190
sunil <sunil@hem.com>
2017-11-24 22:50:59 +0530

    Removed public folder

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 /*
   The below peice of code should always be before routes.
