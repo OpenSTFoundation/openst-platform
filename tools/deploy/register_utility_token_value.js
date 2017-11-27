961198dd436befecf0b19f90a223b9b582e5ff66
pankajkeswani <pankaj@pepo.com>
2017-11-27 20:49:41 +0530

    Add core contract on value chain

--- tools/deploy/register_utility_token_value.js
+++ tools/deploy/register_utility_token_value.js
@@ -9,43 +9,46 @@ const rootPrefix = '../..'
     , coreConstants = require(rootPrefix+'/config/core_constants')
     , coreAddresses = require(rootPrefix+'/config/core_addresses')
     , prompts = readline.createInterface(process.stdin, process.stdout)
-    , deployerAddress = coreAddresses.getAddressForUser(deployerName)
+    , OpenStValueContractInteract = require(rootPrefix+'/lib/contract_interact/openst_value')
+    , populateEnvVars = require(rootPrefix+"/lib/populate_env_vars.js")
     ;
 
+function updateConfig(valueCoreContractAddr) {
+
+    return new Promise( (resolve,reject) => {
+            logger.step("Updating Source file open_st_env_vars");
+    populateEnvVars.renderAndPopulate('valueCore', {
+        ost_value_core_contract_address: valueCoreContractAddr
+    });
+
+})
+.catch( reason =>  {
+        logger.error("Failed to populate open_st_env_vars.sh file!");
+    catchAndExit( reason );
+})
+.then( _ => {
+        logger.win("open_st_env_vars updated.");
+});
+
+};
 
 const performer = async function() {
 
-    console.log("Deployer Address: " + deployerAddress);
     console.log("Value Chain Registrar Address: " + coreAddresses.getAddressForUser("registrar"));
     console.log("Core Contract Address: " + coreAddresses.getAddressForContract('valueCore'));
-
-    // await new Promise(
-    //     function (onResolve, onReject){
-    //         prompts.question("Please verify all above details. Do you want to proceed? [Y/N]", function (intent) {
-    //             if (intent === 'Y') {
-    //                 console.log('Great! Proceeding deployment.');
-    //                 prompts.close();
-    //                 onResolve();
-    //             } else {
-    //                 console.log('Exiting deployment scripts. Change the env vars and re-run.');
-    //                 process.exit(1);
-    //             }
-    //         });
-    //     }
-    // );
 
     // deploy Core contract
     var contractName = 'valueCore'
         ,contractAbi = coreAddresses.getAbiForContract(contractName)
         ,contractBin = coreAddresses.getBinForContract(contractName)
         ,constructorArgs = [
-        coreAddresses.getAddressForContract("valueRegistrar")
+        coreAddresses.getAddressForContract("valueRegistrar"),
         coreConstants.OST_VALUE_CHAIN_ID,
         coreConstants.OST_UTILITY_CHAIN_ID,
         coreAddresses.getAddressesForContract("openSTUtility")]
 
     console.log("Deploying valueCore Contract on ValueChain");
-    var contractDeployTxReceipt = await deployHelper.perform(
+    var coreContractDeployResult = await deployHelper.perform(
         contractName,
         web3Provider,
         contractAbi,
@@ -53,11 +56,22 @@ const performer = async function() {
         deployerName,
         constructorArgs
     );
-    console.log(contractDeployTxReceipt);
+    console.log(coreContractDeployResult);
     console.log(contractName + " Contract deployed ");
 
-    //set ops address to VC registrar addr
+    // Add Core contract address to Environment config
+    const coreContractAddress = coreContractDeployResult.contractAddress;
+    await updateConfig(coreContractAddress);
 
+    // Add core to openst_value
+    const openSTValueContractAddress = coreAddresses.getAddressForContract("openSTValue"),
+        , openStValueContractInteract = new OpenStValueContractInteract(openSTValueContractAddress);
+
+    var addCoreResponse = await openStValueContractInteract.addCore(deployerName, coreContractAddress);
+    console.log(addCoreResponse);
+    console.log("Core Contract " + coreContractAddress + " is added to Value Chain.");
+
+    //set ops address to VC registrar addr
 
 
 };
