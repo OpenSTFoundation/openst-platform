e3faed21047ba757ea6d6a249ab2be8f6d5595c9
rachin <rachin@pepo.com>
2017-11-28 07:52:39 +0530

    undo changes in init_chain.sh

--- test/init_chain.sh
+++ test/init_chain.sh
@@ -3,13 +3,13 @@
 DATADIRVALUE=./st-poa-value
 DATADIRUTILITY=./st-poa-utility
 
-#mkdir -p "$DATADIRVALUE"
+mkdir -p "$DATADIRVALUE"
 mkdir -p "$DATADIRUTILITY"
 
-#rm -rf "$DATADIRVALUE/geth"
+rm -rf "$DATADIRVALUE/geth"
 rm -rf "$DATADIRUTILITY/geth"
 
 
-#geth --datadir "$DATADIRVALUE" init poa-genesis-value.json
+geth --datadir "$DATADIRVALUE" init poa-genesis-value.json
 geth --datadir "$DATADIRUTILITY" init poa-genesis-utility.json
 
