a12a8c60011859e327efcf0f306ea879c20e45d9
kedarchandrayan <kedar@pepo.com>
2017-11-28 10:49:49 +0530

    moving to non empty passphrase.

--- test/run_value_chain.sh
+++ test/run_value_chain.sh
@@ -6,4 +6,4 @@ LOCAL_NETWORK_ID="--networkid 20171011"
 # if no password file, assume empty
 touch pwvalue
 
-geth --datadir "$DATADIR" --rpcport 8545 --gasprice 0 --targetgaslimit 1000000000 --etherbase 0 --unlock 0 --password pwvalue --ws --wsport 18545  --wsorigins "*" --rpc --maxpeers 0 --mine $LOCAL_NETWORK_ID --minerthreads 1 --rpcapi net,eth,web3,personal
+geth --datadir "$DATADIR" --rpcport 8545 --gasprice 0 --targetgaslimit 1000000000 --etherbase 0 --unlock 0 --password pw --ws --wsport 18545  --wsorigins "*" --rpc --maxpeers 0 --mine $LOCAL_NETWORK_ID --minerthreads 1 --rpcapi net,eth,web3,personal
