60d904cf026f7031a630b26b745f37aec177b651
abhay-pepo <abhay@pepo.com>
2017-11-29 21:13:31 +0530

    member key addresses generation using OST_MANAGED_PASSPHRASE

--- test/init_keys.sh
+++ test/init_keys.sh
@@ -5,35 +5,41 @@ ADDRESS_FILE=./new_addresses
 
 echo "Init/Re-Init chain..."
 sh ./init_chain.sh
-echo "testtest" > ./pw
 echo "" > $ADDRESS_FILE
 echo "...Done Init"
 
 
 echo "Generate new addresses..."
+
+echo $OST_FOUNDATION_PASSPHRASE > ./pw
 #Foundation Address
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
 #Registrar Address of Value Chain
+echo $OST_VALUE_REGISTRAR_PASSPHRASE > ./pw
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
 # Value Chain Deployer Address
+echo $OST_VALUE_DEPLOYER_PASSPHRASE > ./pw
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
 # Utility Chain Deployer Address
+echo $OST_UTILITY_DEPLOYER_PASSPHRASE > ./pw
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
 #Ops Address
-geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
-
-#Member Company Address
+echo $OST_VALUE_OPS_PASSPHRASE > ./pw
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
 # OST_UTILITY_CHAIN_OWNER_ADDR Address. Will be used in utility chain
+echo '' > ./pw
 geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
+#Member Company Address
+echo $OST_MANAGED_KEY_PASSPHRASE > ./pw
+geth --datadir "$DATADIRVALUE" account new --password ./pw >> $ADDRESS_FILE
 
-echo "...New addresses generated"
+echo "...New addresses generated"s
 
 echo "Sync keystores"
 #Copy all 
