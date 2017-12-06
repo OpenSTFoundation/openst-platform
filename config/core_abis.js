92e06ed4fba88079b0974d66c7ba2f86d47c964e
rachin <rachin@pepo.com>
2017-12-06 15:54:22 +0530

    fixes #26

--- config/core_abis.js
+++ config/core_abis.js
@@ -5,19 +5,28 @@
  *
  */
 
-const fs = require('fs');
+const fs = require('fs')
+    , path = require('path')
+;
+
+function parseFile( filePath,  options ) {
+    filePath = path.join(__dirname, '/' + filePath );
+    const fileContent = fs.readFileSync( filePath, options || "utf8");
+    return JSON.parse( fileContent );
+}
+
+const rootPrefix = ".."
 
 const coreAbis = {
-    simpleToken: JSON.parse(fs.readFileSync('./contracts/abi/SimpleToken.abi', "utf8")),
-    openSTUtility: JSON.parse(fs.readFileSync('./contracts/abi/OpenSTUtility.abi', "utf8")),
-    openSTValue: JSON.parse(fs.readFileSync('./contracts/abi/OpenSTValue.abi', "utf8")),
-    stPrime: JSON.parse(fs.readFileSync('./contracts/abi/STPrime.abi', "utf8")),
-    valueCore: JSON.parse(fs.readFileSync('./contracts/abi/Core.abi', "utf8")),
-    valueRegistrar: JSON.parse(fs.readFileSync('./contracts/abi/Registrar.abi', "utf8")),
-    utilityRegistrar: JSON.parse(fs.readFileSync('./contracts/abi/Registrar.abi', "utf8")),
-    staking: '',
-    brandedToken: JSON.parse(fs.readFileSync('./contracts/abi/BrandedToken.abi', "utf8")),
-    simpleStake: JSON.parse(fs.readFileSync('./contracts/abi/SimpleStake.abi', "utf8"))
+    simpleToken: parseFile(rootPrefix + '/contracts/abi/SimpleToken.abi', "utf8"),
+    openSTUtility: parseFile( rootPrefix + '/contracts/abi/OpenSTUtility.abi', "utf8"),
+    openSTValue: parseFile( rootPrefix + '/contracts/abi/OpenSTValue.abi', "utf8"),
+    stPrime: parseFile( rootPrefix + '/contracts/abi/STPrime.abi', "utf8"),
+    valueCore: parseFile(rootPrefix + '/contracts/abi/Core.abi', "utf8"),
+    valueRegistrar: parseFile(rootPrefix + '/contracts/abi/Registrar.abi', "utf8"),
+    utilityRegistrar: parseFile(rootPrefix + '/contracts/abi/Registrar.abi', "utf8"),
+    brandedToken: parseFile(rootPrefix + '/contracts/abi/BrandedToken.abi', "utf8"),
+    simpleStake: parseFile( rootPrefix + '/contracts/abi/SimpleStake.abi', "utf8")
   };
 
 module.exports = coreAbis;
--- config/core_bins.js
+++ config/core_bins.js
@@ -5,19 +5,28 @@
  *
  */
 
-const fs = require('fs');
+const fs = require('fs')
+    , path = require('path')
+;
+
+function readFile( filePath,  options ) {
+    filePath = path.join(__dirname, '/' + filePath );
+    return fs.readFileSync( filePath, options || "utf8");
+}
+
+const rootPrefix = "..";
+
 
 const coreBins = {
-  simpleToken: fs.readFileSync('./contracts/bin/SimpleToken.bin', 'utf8'),
-  openSTUtility: fs.readFileSync('./contracts/bin/OpenSTUtility.bin', 'utf8'),
-  openSTValue: fs.readFileSync('./contracts/bin/OpenSTValue.bin', 'utf8'),
-  stPrime: fs.readFileSync('./contracts/bin/STPrime.bin', 'utf8'),
-  valueCore: fs.readFileSync('./contracts/bin/Core.bin', 'utf8'),
-  valueRegistrar: fs.readFileSync('./contracts/bin/Registrar.bin', 'utf8'),
-  utilityRegistrar: fs.readFileSync('./contracts/bin/Registrar.bin', 'utf8'),
-  staking: '',
-  brandedToken:  fs.readFileSync('./contracts/bin/BrandedToken.bin', 'utf8'),
-  simpleStake:  fs.readFileSync('./contracts/bin/SimpleStake.bin', 'utf8')
+  simpleToken:      readFile( rootPrefix + '/contracts/bin/SimpleToken.bin', 'utf8'),
+  openSTUtility:    readFile( rootPrefix + '/contracts/bin/OpenSTUtility.bin', 'utf8'),
+  openSTValue:      readFile( rootPrefix + '/contracts/bin/OpenSTValue.bin', 'utf8'),
+  stPrime:          readFile( rootPrefix + '/contracts/bin/STPrime.bin', 'utf8'),
+  valueCore:        readFile( rootPrefix + '/contracts/bin/Core.bin', 'utf8'),
+  valueRegistrar:   readFile( rootPrefix + '/contracts/bin/Registrar.bin', 'utf8'),
+  utilityRegistrar: readFile( rootPrefix + '/contracts/bin/Registrar.bin', 'utf8'),
+  brandedToken:     readFile( rootPrefix + '/contracts/bin/BrandedToken.bin', 'utf8'),
+  simpleStake:      readFile( rootPrefix + '/contracts/bin/SimpleStake.bin', 'utf8')
 };
 
 module.exports = coreBins;
