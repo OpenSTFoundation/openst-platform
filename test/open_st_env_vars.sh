381db256eb7d14c3ccf014e122b17de48748867f
kedarchandrayan <kedar@pepo.com>
2017-12-06 16:07:25 +0530

    minor.

--- test/open_st_env_vars.sh
+++ test/open_st_env_vars.sh
@@ -1,3 +1,7 @@
+export HTTPS_KEY=''
+export HTTPS_CERT=''
+export HTTPS_PORT=443
+
 export OST_GETH_VALUE_RPC_PROVIDER='http://localhost:8545'
 export OST_GETH_VALUE_WS_PROVIDER='ws://localhost:18545'
 
