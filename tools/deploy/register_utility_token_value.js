9d2f75b7c1065ec6775e91c5f4e94f887c80f0b8
pankajkeswani <pankaj@pepo.com>
2017-12-07 20:53:47 +0530

    Documentation for register utility token on value chain deploy script

--- tools/deploy/register_utility_token_value.js
+++ tools/deploy/register_utility_token_value.js
@@ -1,5 +1,26 @@
 "use strict";
 
+/**
+ * This is a deployer script for registering utility token on Value chain<br><br>
+ *
+ * This deployer script basically deploy core contract on openSTValue.
+ * Steps performed are as listed below:
+ * <ol>
+ *  <li>Deploy ValueCore contract on Value chain by providing ValueRegistrarContract &
+ *  openSTUtilityContract addresses and utility & value chain ids.</li>
+ *  <li>Once ValueCore contract is deployed and we get the contract address will link it with openST_value contract.</li>
+ *  <li>AddCore method of valueRegistrarContract will add deployed core contract over openST_value contract.</li>
+ *  <li>Once the core contract is deployed and added, now is the time to register utility token for STPrime on value chain,
+ *  registerUtilityToken method of valueRegistrarContract will register openSTUtilityContract.</li>
+ *  <li>Set Ops Address to Value Registrar.</li>
+ *  <li>Re-verify that ops user is properly set to value registrar or not.</li>
+ *  <li>Once the ops user is set, then will transfer ownership of ValueRegistrarContract to Simple Token Foundation.</li>
+ *  <li>All above steps once done, will update the config with generated ValueCore contract address.</li>
+ * </ol>
+ *
+ * @module tools/deploy/register_utility_token_value
+ */
+
 const rootPrefix = '../..'
   , readline = require('readline')
   , config = require(rootPrefix + '/config.json')
@@ -30,6 +51,15 @@ const deploymentOptions = {
   gasLimit: coreConstants.OST_VALUE_GAS_LIMIT
 };
 
+
+/**
+ * Updates dynamic addresses generated in the deployment process to a persistant source file
+ * It would be used later on.
+ *
+ * @param {String} valueCoreContractAddr
+ *
+ * @return {}
+ */
 function updateConfig(valueCoreContractAddr) {
 
   return new Promise(
@@ -51,6 +81,14 @@ function updateConfig(valueCoreContractAddr) {
     });
 
 };
+
+/**
+ * It is the main performer method of this deployment script
+ *
+ * @param {}
+ *
+ * @return {}
+ */
 
 const performer = async function () {
 
