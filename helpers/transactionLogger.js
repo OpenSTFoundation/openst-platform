de8251bf7078e6ccbba43f1eb5b4878bd427f400
sunil <sunil@hem.com>
2017-12-11 16:02:39 +0530

    Code Review

--- helpers/transactionLogger.js
+++ helpers/transactionLogger.js
@@ -1,19 +1,18 @@
-
 //All Module Requires.
-const fs    = require('fs')
+const fs = require('fs')
 ;
 
 //All the requires.
-const rootPrefix    = '..'
-  , coreConstants   = require( rootPrefix + '/config/core_constants' )
-  , logger          = require( rootPrefix + '/helpers/custom_console_logger' )
-  , responseHelper  = require( rootPrefix + '/lib/formatter/response')
+const rootPrefix = '..'
+  , coreConstants = require(rootPrefix + '/config/core_constants')
+  , logger = require(rootPrefix + '/helpers/custom_console_logger')
+  , responseHelper = require(rootPrefix + '/lib/formatter/response')
 ;
 
 //All constants.
 const logFolder = coreConstants.OST_TRANSACTION_LOGS_FOLDER
   , _pending = {}
-  , _failed = {}  
+  , _failed = {}
 ;
 
 //All internal methods
@@ -21,30 +20,29 @@ const getLogFilePath = function (memberSymbol, transactionUUID) {
   return logFolder + memberSymbol + '-' + transactionUUID + '.json';
 }
 
-const logProcessor = function ( log, logs, unprocessedLogs ) {
-  if ( !log.length ) {
+const logProcessor = function (log, logs, unprocessedLogs) {
+  if (!log.length) {
     return;
   }
   try {
-    var joLog = JSON.parse( log );
-    logs.push( joLog );
+    var joLog = JSON.parse(log);
+    logs.push(joLog);
     var strData = joLog.data;
     try {
-      joLog.data = JSON.parse( strData );
-    } catch (ex ) {
+      joLog.data = JSON.parse(strData);
+    } catch (ex) {
       //Ignore it. The data can be string.
     }
-  } catch( ex ) {
-    unprocessedLogs.push( log );
+  } catch (ex) {
+    unprocessedLogs.push(log);
     logger.error("TransactionLogger :: getTransactionLogs :: failed to decode log. log:", log);
-    logger.error( ex );
+    logger.error(ex);
   }
 };
 
 
-
 //Constructor.
-const TransactionLogger = module.exports = function ( transactionParams, memberSymbol ) {
+const TransactionLogger = module.exports = function (transactionParams, memberSymbol) {
 
   this.transactionParams = transactionParams;
 
@@ -96,31 +94,30 @@ TransactionLogger.prototype = {
   writeToFile: function (args, logLevel) {
 
     const oThis = this
-        , time = oThis.currentTime();
-    ;
+      , time = oThis.currentTime();
 
     Object.values(args).forEach(function (arg) {
       var formattedLine = oThis.formatLineforLogFile(arg, time, logLevel);
       fs.appendFile(oThis.filePath, formattedLine, (err) => {
         if (err) {
           logger.error("couldn't append to log file" + err);
-        };
+        }
       });
     });
 
   },
 
-  currentTime: function() {
+  currentTime: function () {
     return new Date();
   },
 
-  formatLineforLogFile: function(data, time, logLevel) {
-    if ( typeof data != 'string' ) {
-       if ( data instanceof Object ) {
-         data = JSON.stringify( data );
-       } else {
-         data = String( data );
-       }
+  formatLineforLogFile: function (data, time, logLevel) {
+    if (typeof data !== 'string') {
+      if (data instanceof Object) {
+        data = JSON.stringify(data);
+      } else {
+        data = String(data);
+      }
     }
     var buffer = {
       "time": time,
@@ -131,25 +128,25 @@ TransactionLogger.prototype = {
   },
 
   markPending: function () {
-    _pending[ this.transactionParams.transactionUUID ] = true;
+    _pending[this.transactionParams.transactionUUID] = true;
   },
 
   markSuccess: function () {
-    _pending[ this.transactionParams.transactionUUID ] = false;
-    delete _pending[ this.transactionParams.transactionUUID ];
+    _pending[this.transactionParams.transactionUUID] = false;
+    delete _pending[this.transactionParams.transactionUUID];
     this._logComplete("Transaction Success");
   },
 
   markFailed: function () {
-    _failed[ this.transactionParams.transactionUUID ] = true;
-    _pending[ this.transactionParams.transactionUUID ] = false;
-    delete _pending[ this.transactionParams.transactionUUID ];
+    _failed[this.transactionParams.transactionUUID] = true;
+    _pending[this.transactionParams.transactionUUID] = false;
+    delete _pending[this.transactionParams.transactionUUID];
     this._logComplete("Transaction Failed");
   }
 }
 
 //All Static Methods. Note: Static methods can not be called on instance.
-TransactionLogger.getTransactionLogs = function(memberSymbol, transactionUUID, callback) {
+TransactionLogger.getTransactionLogs = function (memberSymbol, transactionUUID, callback) {
 
   const filePath = getLogFilePath(memberSymbol, transactionUUID);
 
@@ -157,126 +154,126 @@ TransactionLogger.getTransactionLogs = function(memberSymbol, transactionUUID, c
     var response = null;
     if (err) {
       logger.error('error reading filePath: ', filePath, 'error : ', err);
-      response =  responseHelper.error('h_tl_1', "Invalid transactionUUID");
-      }
+      response = responseHelper.error('h_tl_1', "Invalid transactionUUID");
+    }
     else {
       var dataCopy = data;
-      dataCopy = dataCopy.replace(/\r/gm , "");
+      dataCopy = dataCopy.replace(/\r/gm, "");
       var stringLogs = dataCopy.split(/\n/);
       var logs = [];
       var unprocessedLogs = [];
-      stringLogs.forEach( function ( log ) {
-        return logProcessor( log, logs, unprocessedLogs);
+      stringLogs.forEach(function (log) {
+        return logProcessor(log, logs, unprocessedLogs);
       });
 
       var returnObj = {
-        'logs' : logs,
+        'logs': logs,
         "transactionUUID": transactionUUID,
         "memberSymbol": memberSymbol
       };
-      if ( unprocessedLogs.length ) {
-        returnObj[ "unprocessed" ] = unprocessedLogs;
+      if (unprocessedLogs.length) {
+        returnObj["unprocessed"] = unprocessedLogs;
       }
-      response =  responseHelper.successWithData( returnObj );
+      response = responseHelper.successWithData(returnObj);
     }
     callback && callback(response);
   });
 
 };
 
-TransactionLogger.getPendingTransactions = function (memberSymbol, callback ) {
+TransactionLogger.getPendingTransactions = function (memberSymbol, callback) {
   var filtered = [];
-  Object.keys( _pending ).forEach( uuid => {
+  Object.keys(_pending).forEach(uuid => {
     //To-Do: Validate memberSymbol.
-    if ( _pending[ uuid ] ) {
+    if (_pending[uuid]) {
       //Is still pending?
-      filtered.push( uuid );
+      filtered.push(uuid);
     }
   });
-  response = responseHelper.successWithData( { "pending_transaction_uuids" : filtered} );
-  callback && callback( response );
+  response = responseHelper.successWithData({"pending_transaction_uuids": filtered});
+  callback && callback(response);
 };
 
 
-TransactionLogger.getFailedTransactions = function (memberSymbol, callback ) {
+TransactionLogger.getFailedTransactions = function (memberSymbol, callback) {
   var filtered = [];
-  Object.keys( _failed ).forEach( uuid => {
+  Object.keys(_failed).forEach(uuid => {
     //To-Do: Validate memberSymbol.
-    if ( _pending[ uuid ] ) {
+    if (_pending[uuid]) {
       //Is still pending?
-      filtered.push( uuid );
+      filtered.push(uuid);
     }
   });
-  response = responseHelper.successWithData( {"failed_transaction_uuids": filtered} );
-  callback && callback( response );
+  response = responseHelper.successWithData({"failed_transaction_uuids": filtered});
+  callback && callback(response);
 };
 
 TransactionLogger.debug = {
 
 
-  "findAllPending": function ( memberSymbol ) {
+  "findAllPending": function (memberSymbol) {
     const oThis = this;
 
     memberSymbol = memberSymbol || "";
 
-    if ( !memberSymbol.length ) {
+    if (!memberSymbol.length) {
       console.log("Please specify memberSymbol");
       return;
     }
 
     const symPrefix = memberSymbol + "-";
     const allPromises = [];
-    fs.readdir( logFolder, function (err, fileNames) {
-        // "files" is an Array with files names
-        if (err) {
-          logger.error("Something went wrong");
-          logger.error( err );
+    fs.readdir(logFolder, function (err, fileNames) {
+      // "files" is an Array with files names
+      if (err) {
+        logger.error("Something went wrong");
+        logger.error(err);
+        return;
+      }
+
+      fileNames.forEach(fileName => {
+        if (!fileName.startsWith(memberSymbol)) {
           return;
         }
 
-        fileNames.forEach( fileName => {
-          if ( !fileName.startsWith( memberSymbol ) ) {
-            return;
-          }
+        var transactionUUID = fileName.replace(symPrefix, "").replace(".json", "");
 
-          var transactionUUID = fileName.replace( symPrefix, "" ).replace(".json" , "");
-
-          const txPromise = new Promise( (resolve, reject) => {
-            TransactionLogger.getTransactionLogs(memberSymbol, transactionUUID, function ( response ) {
-              oThis.processTransactionLog(response, resolve, reject);
-            });
+        const txPromise = new Promise((resolve, reject) => {
+          TransactionLogger.getTransactionLogs(memberSymbol, transactionUUID, function (response) {
+            oThis.processTransactionLog(response, resolve, reject);
           });
-
-          allPromises.push( txPromise );          
-
         });
 
-        Promise.all( allPromises )
-          .then(values => {
-            logger.win("============= All logs processed =============");
-          })
-        ;
+        allPromises.push(txPromise);
+
+      });
+
+      Promise.all(allPromises)
+        .then(values => {
+          logger.win("============= All logs processed =============");
+        })
+      ;
     });
 
 
   },
 
-  "processTransactionLog": function ( getLogsResponse, resolve ) {
+  "processTransactionLog": function (getLogsResponse, resolve) {
     const oThis = this;
 
-    if ( !getLogsResponse.isSuccess() ) {
+    if (!getLogsResponse.isSuccess()) {
       logger.error("Failed to read logs");
-      logger.info( JSON.stringify( getLogsResponse ) );
+      logger.info(JSON.stringify(getLogsResponse));
       resolve("error");
       return;
     }
 
     const logs = getLogsResponse.data.logs;
-    const completeLog = logs.find( log => {
+    const completeLog = logs.find(log => {
       return log.logLevel === "COMPLETE";
     });
 
-    if ( !completeLog ) {
+    if (!completeLog) {
       logger.info("Incomlete log found. transactionUUID = ", getLogsResponse.data.transactionUUID);
     }
     resolve("done");
