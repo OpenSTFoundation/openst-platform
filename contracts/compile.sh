907bcf6e931c1ea764c1bfbddb82170c67bb65a5
rachin <rachin@pepo.com>
2017-12-12 16:10:52 +0530

    optimised contracts.

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -10,33 +10,33 @@ echo ""
 echo "Compiling SimpleToken.sol"
 echo ""
 
-solc --abi --overwrite ../openst-protocol/contracts/SimpleToken/SimpleToken.sol -o $ABIDIRUTILITY
-solc --bin --overwrite ../openst-protocol/contracts/SimpleToken/SimpleToken.sol -o $BINDIRVALUE
+solc --abi --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/SimpleToken/SimpleToken.sol -o $ABIDIRUTILITY
+solc --bin --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/SimpleToken/SimpleToken.sol -o $BINDIRVALUE
 
 echo ""
 echo "Compiling OpenSTUtility.sol"
 echo ""
 
-solc --abi --overwrite ../openst-protocol/contracts/OpenSTUtility.sol -o $ABIDIRUTILITY
-solc --bin --overwrite ../openst-protocol/contracts/OpenSTUtility.sol -o $BINDIRVALUE
+solc --abi --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/OpenSTUtility.sol -o $ABIDIRUTILITY
+solc --bin --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/OpenSTUtility.sol -o $BINDIRVALUE
 
 echo ""
 echo "Compiling OpenSTValue.sol"
 echo ""
 
-solc --abi --overwrite ../openst-protocol/contracts/OpenSTValue.sol -o $ABIDIRUTILITY
-solc --bin --overwrite ../openst-protocol/contracts/OpenSTValue.sol -o $BINDIRVALUE
+solc --abi --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/OpenSTValue.sol -o $ABIDIRUTILITY
+solc --bin --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/OpenSTValue.sol -o $BINDIRVALUE
 
 echo ""
 echo "Compiling Core.sol"
 echo ""
 
-solc --abi --overwrite ../openst-protocol/contracts/Core.sol -o $ABIDIRUTILITY
-solc --bin --overwrite ../openst-protocol/contracts/Core.sol -o $BINDIRVALUE
+solc --abi --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/Core.sol -o $ABIDIRUTILITY
+solc --bin --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/Core.sol -o $BINDIRVALUE
 
 echo ""
 echo "Compiling Registrar.sol"
 echo ""
 
-solc --abi --overwrite ../openst-protocol/contracts/Registrar.sol -o $ABIDIRUTILITY
-solc --bin --overwrite ../openst-protocol/contracts/Registrar.sol -o $BINDIRVALUE
+solc --abi --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/Registrar.sol -o $ABIDIRUTILITY
+solc --bin --optimize --optimize-runs 200 --overwrite ../openst-protocol/contracts/Registrar.sol -o $BINDIRVALUE
