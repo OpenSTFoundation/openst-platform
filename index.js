81e8fb1a019727b1f5708984480476ffd6a3b3ed
kedarchandrayan <kedar@pepo.com>
2018-01-18 17:31:22 +0530

    address related services.

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 
 "use strict";
 
-const reqPrefix = "."
-  , version = require(reqPrefix + '/package.json').version
-  , btContract = require(reqPrefix + "/lib/contract_interact/branded_token")
-  , transactionLogger = require(reqPrefix + "/helpers/transactionLogger");
+const rootPrefix = "."
+  , version = require(rootPrefix + '/package.json').version
+  , btContract = require(rootPrefix + "/lib/contract_interact/branded_token")
+  , transactionLogger = require(rootPrefix + "/helpers/transactionLogger")
+  , address = require(rootPrefix + '/services/address');
 
 var OpenSTPlatform = function () {
   var oThis = this;
@@ -16,6 +17,9 @@ var OpenSTPlatform = function () {
 
   oThis.contracts = {};
   oThis.contracts.brandedToken = btContract;
+
+  // address related services.
+  oThis.address = address;
 
   oThis.helpers = {};
   oThis.helpers.transactionLogger = transactionLogger;
---
+++ services/address.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Address related services.
+ */
+
+const rootPrefix = '..'
+  ;
+
+const address = {
+  // creates an address with keystore file on the correct chain, using the passphrase which is given in params.
+  create: async function (chain, passphrase) {
+    var web3RpcProvider = null;
+    if (chain == 'utility') {
+      web3RpcProvider = require(rootPrefix + '/lib/web3/providers/utility_rpc')
+    } else {
+      web3RpcProvider = require(rootPrefix + '/lib/web3/providers/value_rpc')
+    }
+
+    // returns a promise which resolves to an address which was created.
+    return web3RpcProvider.eth.personal.newAccount(passphrase);
+  }
+};
+
+module.exports = address;
