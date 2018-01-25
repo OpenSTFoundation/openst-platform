ec11fe42f0aebe0aae2f4cedb6caf5dcba2b0c48
pankajkeswani <pankaj@pepo.com>
2018-01-25 14:13:48 +0530

    Create new address on chains

--- services/address.js
+++ services/address.js
@@ -5,20 +5,21 @@
  */
 
 const rootPrefix = '..'
+  , web3ProviderFactory = require(rootPrefix + '/lib/web3/providers/factory')
   ;
 
 const address = {
   // creates an address with keystore file on the correct chain, using the passphrase which is given in params.
   create: function (chain, passphrase) {
-    var web3RpcProvider = null;
-    if (chain == 'utility') {
-      web3RpcProvider = require(rootPrefix + '/lib/web3/providers/utility_rpc')
-    } else {
-      web3RpcProvider = require(rootPrefix + '/lib/web3/providers/value_rpc')
+
+    const web3Provider = web3ProviderFactory.getProvider(chain, web3ProviderFactory.typeRPC);
+    if(!web3Provider) {
+      // this is a error scenario.
+      return Promise.reject('Invalid chain.');
     }
 
     // returns a promise which resolves to an address which was created.
-    return web3RpcProvider.eth.personal.newAccount(passphrase);
+    return web3Provider.eth.personal.newAccount(passphrase);
   }
 };
 
