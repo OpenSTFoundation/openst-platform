b5a027e2383b9eec3bc4cfd01b5c39eb89c74bc1
kedarchandrayan <kedar@pepo.com>
2018-02-05 21:31:29 +0530

    doc commit.

--- helpers/custom_console_logger.js
+++ helpers/custom_console_logger.js
@@ -1,35 +1,33 @@
 "use strict";
 
-/*
- * Custom Console log methods. Apply different colors for different log levels/severity.
+/**
+ * Custom console logger
  *
+ * @module helpers/custom_console_logger
  */
 
-const CONSOLE_RESET = "\x1b[0m";
-const ERR_PRE = "\x1b[31m"; //Error. (RED)
-const INFO_PRE = "\x1b[33m  "; //Info (YELLOW)
-const WIN_PRE = "\x1b[32m"; //Success (GREEN)
-const WARN_PRE = "\x1b[43m";
-const STEP_PRE = "\n\x1b[34m"; //Step Description (BLUE)
+const getNamespace = require('continuation-local-storage').getNamespace
+;
 
-
-// const CONSOLE_RESET = "";
-// const ERR_PRE = "";
-// const INFO_PRE = "";
-// const WIN_PRE = "";
-// const WARN_PRE = "";
-// const STEP_PRE = "";
+const CONSOLE_RESET = "\x1b[0m"
+  , ERR_PRE = "\x1b[31m" //Error. (RED)
+  , INFO_PRE = "\x1b[33m  " //Info (YELLOW)
+  , WIN_PRE = "\x1b[32m" //Success (GREEN)
+  , WARN_PRE = "\x1b[43m"
+  , STEP_PRE = "\n\x1b[34m"
+;
 
 // Get common local storage namespace to read
 // request identifiers for debugging and logging
-var getNamespace = require('continuation-local-storage').getNamespace
-  , requestNamespace = getNamespace('inputRequest')
+const requestNamespace = getNamespace('inputRequest')
 ;
 
 /**
  * Method to append Request in each log line.
+ *
+ * @param {string} message
  */
-var appendRequest = function(message) {
+const appendRequest = function(message) {
     var newMessage = "";
     if (requestNamespace) {
       if (requestNamespace.get('reqId')) {
@@ -38,7 +36,7 @@ var appendRequest = function(message) {
       if (requestNamespace.get('workerId')) {
         newMessage += "[Worker - " + requestNamespace.get('workerId') + "]";
       }
-      var hrTime = process.hrtime();
+      const hrTime = process.hrtime();
       newMessage += "[" + timeInMilli(hrTime) + "]";
     }
     newMessage += message;
@@ -47,31 +45,80 @@ var appendRequest = function(message) {
 
 /**
  * Method to convert Process hrTime to Milliseconds
+ *
+ * @param {number} hrTime - this is the time in hours
+ *
+ * @return {number} - returns time in milli seconds
  */
-var timeInMilli = function(hrTime) {
+const timeInMilli = function(hrTime) {
   return (hrTime[0] * 1000 + hrTime[1] / 1000000);
 };
 
 /**
  * Find out the difference between request start time and complete time
  */
-var calculateRequestTime = function() {
+const calculateRequestTime = function() {
   var reqTime = 0;
   if (requestNamespace && requestNamespace.get('startTime')) {
-    var hrTime = process.hrtime();
+    const hrTime = process.hrtime();
     reqTime = timeInMilli(hrTime) - timeInMilli(requestNamespace.get('startTime'));
   }
   return reqTime;
 };
 
-module.exports = {
-  "STEP_PRE": STEP_PRE,
-  "WARN_PRE": WARN_PRE,
-  "WIN_PRE": WIN_PRE,
-  "INFO_PRE": INFO_PRE,
-  "ERR_PRE": ERR_PRE,
-  "CONSOLE_RESET": CONSOLE_RESET,
+/**
+ * Custom COnsole Logger
+ *
+ * @constructor
+ */
+const CustomConsoleLoggerKlass = function() {};
 
+CustomConsoleLoggerKlass.prototype = {
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  STEP_PRE: STEP_PRE,
+
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  WARN_PRE: WARN_PRE,
+
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  WIN_PRE: WIN_PRE,
+
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  INFO_PRE: INFO_PRE,
+
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  ERR_PRE: ERR_PRE,
+
+  /**
+   * @ignore
+   *
+   * @constant {string}
+   */
+  CONSOLE_RESET: CONSOLE_RESET,
+
+  /**
+   * Log step
+   */
   step: function () {
     var args = [appendRequest(this.STEP_PRE)];
     args = args.concat(Array.prototype.slice.call(arguments));
@@ -79,7 +126,9 @@ module.exports = {
     console.log.apply(console, args);
   },
 
-  //Method to Log Information
+  /**
+   * Log info
+   */
   info: function () {
     var args = [appendRequest(this.INFO_PRE)];
     args = args.concat(Array.prototype.slice.call(arguments));
@@ -87,7 +136,9 @@ module.exports = {
     console.log.apply(console, args);
   },
 
-  //Method to Log Error.
+  /**
+   * Log error
+   */
   error: function () {
     var args = [appendRequest(this.ERR_PRE)];
     args = args.concat(Array.prototype.slice.call(arguments));
@@ -95,6 +146,9 @@ module.exports = {
     console.log.apply(console, args);
   },
 
+  /**
+   * Log warn
+   */
   warn: function () {
     var args = [appendRequest(this.WARN_PRE)];
     args = args.concat(Array.prototype.slice.call(arguments));
@@ -102,7 +156,9 @@ module.exports = {
     console.log.apply(console, args);
   },
 
-  //Method to Log Success/Win.
+  /**
+   * Log win - on done
+   */
   win: function () {
     var args = [appendRequest(this.WIN_PRE)];
     args = args.concat(Array.prototype.slice.call(arguments));
@@ -110,22 +166,34 @@ module.exports = {
     console.log.apply(console, args);
   },
 
+  /**
+   * Log normal level
+   */
   log: function () {
     console.log.apply(console, arguments);
   },
 
   //Method to Log Request Started.
   requestStartLog: function (requestUrl) {
-    var d = new Date();
-    var dateTime = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+"."+d.getMilliseconds();
-    var message = "Started '" + requestUrl + "' at " + dateTime;
-    this.info(message);
+    const oThis = this
+      , d = new Date()
+      , dateTime = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + " " + d.getHours() + ":" +
+                    d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds()
+      , message = 'Started \'' + requestUrl + '\' at ' + dateTime
+    ;
+
+    oThis.info(message);
   },
 
   //Method to Log Request Completed.
   requestCompleteLog: function (status) {
-    var message = "Completed '" + status + "' in " + calculateRequestTime() + "ms";
-    this.info(message);
+    const oThis = this
+      , message = 'Completed \'' + status + '\' in ' + calculateRequestTime() + 'ms'
+    ;
+
+    oThis.info(message);
   }
 
 };
+
+module.exports = new CustomConsoleLoggerKlass();
