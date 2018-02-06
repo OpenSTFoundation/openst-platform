8ce4bd73d12b114c1e2b7a6950c2ee8832ffd059
sunil <sunil@hem.com>
2018-02-06 20:06:04 +0530

    Reduced miners from default to 1

--- tools/setup/service_manager.js
+++ tools/setup/service_manager.js
@@ -163,7 +163,7 @@ ServiceManagerKlass.prototype = {
     return "geth --networkid " + networkId + " --datadir " + chainDataDir + " --port " + chainPort +
       " --rpc --rpcapi eth,net,web3,personal --rpcport " + rpcPort + " --rpcaddr " + rpcHost + " --ws" +
       " --wsport " + wsPort + " --wsorigins '*' --wsaddr " + wsHost + " --etherbase " + sealerAddr +
-      " --mine --targetgaslimit " + gasLimit[chain] + "  --gasprice \"" + gasPrice + "\" --unlock " +
+      " --mine --minerthreads 1 --targetgaslimit " + gasLimit[chain] + "  --gasprice \"" + gasPrice + "\" --unlock " +
       sealerAddr + " --password "+ chainDataDir + "/" + sealerPassphraseFile;
   }
 
