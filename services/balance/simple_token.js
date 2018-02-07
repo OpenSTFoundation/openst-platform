c0897eeb45d62810afdc35f87506ce4c0b01b9ed
Alpesh <alpesh@pepo.com>
2018-02-07 13:35:14 +0530

    simple token balance service.

---
+++ services/balance/simple_token.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * Get simple token Balance of an address
+ *
+ * @module services/balance/simple_token
+ */
+
+const rootPrefix = '../..'
+  , fundManager = require(rootPrefix + '/lib/fund_manager')
+  , responseHelper = require(rootPrefix + '/lib/formatter/response')
+;
+
+/**
+ * Eth balance
+ *
+ * @param {object} params - this is object with keys - address
+ *
+ * @constructor
+ */
+const SimpleTokenBalanceKlass = function(params) {
+  this.address = params.address;
+};
+
+SimpleTokenBalanceKlass.prototype = {
+
+  perform: function () {
+    var oThis = this;
+
+    try {
+      return fundManager.getSTBalanceOf(oThis.address);
+    } catch (err) {
+      return Promise.resolve(responseHelper.error('s_b_st_1', 'Something went wrong. ' + err.message));
+    }
+
+  }
+
+};
+
+module.exports = SimpleTokenBalanceKlass;
