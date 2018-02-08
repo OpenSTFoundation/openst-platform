d4410f3a7daa1fbeb30b780e3880645595de3ed3
Alpesh <alpesh@pepo.com>
2018-02-08 21:53:01 +0530

    subscribe to openst notification changes.

--- executables/notification_subscribe.js
+++ executables/notification_subscribe.js
@@ -2,9 +2,18 @@
 
 const openSTNotification = require('@openstfoundation/openst-notification');
 
-openSTNotification.subscribe_event.rabbit(
-  ['#'],
-  function(msgContent){
-    console.log('[RECEIVED]', msgContent, '\n')
-  }
-);
+
+function subscribe(){
+  openSTNotification.subscribe_event.rabbit(
+    ['#'],
+    function(msgContent){
+      console.log('[RECEIVED]', msgContent, '\n')
+    }
+  );
+}
+subscribe();
+
+openSTNotification.subscribe_event.local(['rmq_fail'], function(err){
+  console.log('RMQ Failed event received.');
+  setTimeout(subscribe, 2000);
+});
