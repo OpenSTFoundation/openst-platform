f75711a76d476a5c1fa63ee4d154dc0a10637652
Alpesh <alpesh@pepo.com>
2018-02-12 12:18:30 +0530

    subscribe call change.

--- executables/notification_subscribe.js
+++ executables/notification_subscribe.js
@@ -1,14 +1,21 @@
 "use strict";
 
+/**
+ * Executable to listen rabbitmq events.
+ */
+
 const openSTNotification = require('@openstfoundation/openst-notification');
 
 const rootPrefix = '..'
   , logger = require(rootPrefix + '/helpers/custom_console_logger');
 
-
+/**
+ * Subscribe to all events of rabbit.
+ */
 function subscribe(){
   openSTNotification.subscribeEvent.rabbit(
     ['#'],
+    {queue: 'openst_platform'},
     function(msgContent){
       logger.info('[RECEIVED]', msgContent, '\n');
     }
