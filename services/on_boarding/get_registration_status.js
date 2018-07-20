50a0d251d76b17cce37d27928b66c3f1cd5f7ff0
alpeshvmodi <alpeshvmodi@gmail.com>
2018-07-20 12:29:36 +0530

    async onboarding

--- services/on_boarding/get_registration_status.js
+++ services/on_boarding/get_registration_status.js
@@ -12,6 +12,7 @@ const rootPrefix = '../..'
   , responseHelper = require(rootPrefix + '/lib/formatter/response')
   , RegistrationStatusKlass = require(rootPrefix + '/helpers/registration_status')
   , basicHelper = require(rootPrefix + '/helpers/basic_helper')
+  , logger = require(rootPrefix + '/helpers/custom_console_logger')
 ;
 
 require(rootPrefix + '/lib/web3/providers/factory');
@@ -46,94 +47,110 @@ const GetRegistrationStatusKlass = function (params) {
 GetRegistrationStatusKlass.prototype = {
 
   /**
-   * Perform<br><br>
+   * Perform
    *
-   * @return {promise<result>} - returns a promise which resolves to an object of kind Result
+   * @return {promise<result>}
    */
-  perform: async function () {
+  perform: function () {
+    const oThis = this;
+
+    return oThis.asyncPerform()
+      .catch(function (error) {
+        if (responseHelper.isCustomResult(error)) {
+          return error;
+        } else {
+          logger.error('openst-platform::services/on_boarding/get_registration_status.js::perform::catch');
+          logger.error(error);
+
+          return responseHelper.error({
+            internal_error_identifier: 's_ob_grs_3',
+            api_error_identifier: 'something_went_wrong',
+            error_config: basicHelper.fetchErrorConfig(),
+            debug_options: {err: error}
+          });
+        }
+      });
+  },
+
+  /**
+   * Async Perform
+   *
+   * @return {promise<result>}
+   */
+  asyncPerform: async function () {
     const oThis = this
     ;
 
-    try {
-      // validations
-      if (!basicHelper.isTxHashValid(oThis.transactionHash)) {
-        let errObj = responseHelper.error({
-          internal_error_identifier: 's_ob_grs_1',
-          api_error_identifier: 'invalid_transaction_hash',
-          error_config: basicHelper.fetchErrorConfig()
-        });
-
-        return Promise.resolve(errObj);
-      }
-
-      // returns the registration status of the proposal
-      const registrationStatus = new RegistrationStatusKlass()
-      ;
-
-      let web3ProviderFactory = oThis.ic().getWeb3ProviderFactory();
-      const web3Provider = web3ProviderFactory.getProvider('utility', web3ProviderFactory.typeWS);
-
-      let TransactionReceiptServiceKlass = oThis.ic().getTransactionReceiptService();
-
-      // check if the proposal transaction is mined
-      const getReceiptObj = new TransactionReceiptServiceKlass({transaction_hash: oThis.transactionHash, chain: web3Provider.chainKind});
-      const proposalTxReceiptResponse = await getReceiptObj.perform();
-
-      // if error or transaction not yet mined or transaction failed, return. Else proceed and check for other things
-      if (!proposalTxReceiptResponse.isSuccess() || !proposalTxReceiptResponse.data.formattedTransactionReceipt) {
-        return registrationStatus.returnResultPromise();
-      }
-
-      registrationStatus.setIsProposalDone(1);
-
-      const proposalFormattedTxReceipt = proposalTxReceiptResponse.data.formattedTransactionReceipt;
-      const proposalFormattedEvents = await web3EventsFormatter.perform(proposalFormattedTxReceipt);
-
-      const uuid = proposalFormattedEvents['ProposedBrandedToken']['_uuid'];
-      registrationStatus.setUuid(uuid);
-
-      // now checking to confirm if registration on UC took place
-      let OpenStUtilityContractInteractKlass = oThis.ic().getOpenSTUtilityeInteractClass(),
-        openSTUtilityContractInteract = new OpenStUtilityContractInteractKlass(oThis.openStUtilityContractAddr)
-      ;
-
-      const registeredOnUCResponse = await openSTUtilityContractInteract.registeredToken(uuid);
-
-      if (!registeredOnUCResponse ||
-        !registeredOnUCResponse.isSuccess() ||
-        (registeredOnUCResponse.data.erc20Address == '0x0000000000000000000000000000000000000000')) {
-        return registrationStatus.returnResultPromise();
-      }
-
-      registrationStatus.setIsRegisteredOnUc(1);
-      registrationStatus.setErc20Address(registeredOnUCResponse.data.erc20Address);
-
-      // now checking to confirm if registration on VC took place
-      let OpenStValueContractInteractKlass = oThis.ic().getOpenSTValueInteractClass(),
-        openSTValueContractInteract = new OpenStValueContractInteractKlass(oThis.openStValueContractAddr)
-      ;
-
-      const registeredOnVCResponse = await openSTValueContractInteract.utilityTokens(uuid);
-
-      if (!registeredOnVCResponse ||
-        !registeredOnVCResponse.isSuccess() ||
-        (registeredOnVCResponse.data.symbol.length == 0)) {
-        return registrationStatus.returnResultPromise();
-      }
-
-      registrationStatus.setIsRegisteredOnVc(1);
-
-      return registrationStatus.returnResultPromise();
-    } catch (err) {
+    // validations
+    if (!basicHelper.isTxHashValid(oThis.transactionHash)) {
       let errObj = responseHelper.error({
-        internal_error_identifier: 's_ob_grs_3',
-        api_error_identifier: 'something_went_wrong',
-        error_config: basicHelper.fetchErrorConfig(),
-        debug_options: {err: err}
+        internal_error_identifier: 's_ob_grs_1',
+        api_error_identifier: 'invalid_transaction_hash',
+        error_config: basicHelper.fetchErrorConfig()
       });
 
       return Promise.resolve(errObj);
     }
+
+    // returns the registration status of the proposal
+    const registrationStatus = new RegistrationStatusKlass()
+    ;
+
+    let web3ProviderFactory = oThis.ic().getWeb3ProviderFactory();
+    const web3Provider = web3ProviderFactory.getProvider('utility', web3ProviderFactory.typeWS);
+
+    let TransactionReceiptServiceKlass = oThis.ic().getTransactionReceiptService();
+
+    // check if the proposal transaction is mined
+    const getReceiptObj = new TransactionReceiptServiceKlass({transaction_hash: oThis.transactionHash, chain: web3Provider.chainKind});
+    const proposalTxReceiptResponse = await getReceiptObj.perform();
+
+    // if error or transaction not yet mined or transaction failed, return. Else proceed and check for other things
+    if (!proposalTxReceiptResponse.isSuccess() || !proposalTxReceiptResponse.data.formattedTransactionReceipt) {
+      return registrationStatus.returnResultPromise();
+    }
+
+    registrationStatus.setIsProposalDone(1);
+
+    const proposalFormattedTxReceipt = proposalTxReceiptResponse.data.formattedTransactionReceipt;
+    const proposalFormattedEvents = await web3EventsFormatter.perform(proposalFormattedTxReceipt);
+
+    const uuid = proposalFormattedEvents['ProposedBrandedToken']['_uuid'];
+    registrationStatus.setUuid(uuid);
+
+    // now checking to confirm if registration on UC took place
+    let OpenStUtilityContractInteractKlass = oThis.ic().getOpenSTUtilityeInteractClass(),
+      openSTUtilityContractInteract = new OpenStUtilityContractInteractKlass(oThis.openStUtilityContractAddr)
+    ;
+
+    const registeredOnUCResponse = await openSTUtilityContractInteract.registeredToken(uuid);
+
+    if (!registeredOnUCResponse ||
+      !registeredOnUCResponse.isSuccess() ||
+      (registeredOnUCResponse.data.erc20Address == '0x0000000000000000000000000000000000000000')) {
+      return registrationStatus.returnResultPromise();
+    }
+
+    registrationStatus.setIsRegisteredOnUc(1);
+    registrationStatus.setErc20Address(registeredOnUCResponse.data.erc20Address);
+
+    // now checking to confirm if registration on VC took place
+    let OpenStValueContractInteractKlass = oThis.ic().getOpenSTValueInteractClass(),
+      openSTValueContractInteract = new OpenStValueContractInteractKlass(oThis.openStValueContractAddr)
+    ;
+
+    const registeredOnVCResponse = await openSTValueContractInteract.utilityTokens(uuid);
+
+    if (!registeredOnVCResponse ||
+      !registeredOnVCResponse.isSuccess() ||
+      (registeredOnVCResponse.data.symbol.length == 0)) {
+      return registrationStatus.returnResultPromise();
+    }
+
+    registrationStatus.setIsRegisteredOnVc(1);
+
+    return registrationStatus.returnResultPromise();
+
   }
 };
 
--- services/on_boarding/propose_branded_token.js
+++ services/on_boarding/propose_branded_token.js
@@ -11,6 +11,7 @@ const rootPrefix = '../..'
   , InstanceComposer = require(rootPrefix + "/instance_composer")
   , responseHelper = require(rootPrefix + '/lib/formatter/response')
   , basicHelper = require(rootPrefix + '/helpers/basic_helper')
+  , logger = require(rootPrefix + '/helpers/custom_console_logger')
 ;
 
 require(rootPrefix + '/config/core_addresses');
@@ -50,82 +51,97 @@ const ProposeBrandedTokenKlass = function (params) {
 };
 
 ProposeBrandedTokenKlass.prototype = {
+
   /**
-   * Perform<br><br>
+   * Perform
    *
-   * @return {promise<result>} - returns a promise which resolves to an object of kind Result
+   * @return {promise<result>}
    */
-  perform: async function () {
+  perform: function () {
+    const oThis = this;
+
+    return oThis.asyncPerform()
+      .catch(function (error) {
+        logger.error('openst-platform::services/on_boarding/propose_branded_token.js::perform::catch');
+        logger.error(error);
+
+        if (responseHelper.isCustomResult(error)) {
+          return error;
+        } else {
+          return responseHelper.error({
+            internal_error_identifier: 's_ob_pbt_4',
+            api_error_identifier: 'something_went_wrong',
+            error_config: basicHelper.fetchErrorConfig(),
+            debug_options: {err: error}
+          });
+        }
+      });
+  },
+
+  /**
+   * Async Perform
+   *
+   * @return {promise<result>}
+   */
+  asyncPerform: async function () {
     const oThis = this
     ;
 
-    try {
-      //validations
-      if (!basicHelper.isBTNameValid(oThis.name)) {
-        let errObj = responseHelper.error({
-          internal_error_identifier: 's_ob_pbt_1',
-          api_error_identifier: 'invalid_branded_token_name',
-          error_config: basicHelper.fetchErrorConfig()
-        });
-
-        return Promise.resolve(errObj);
-      }
-      if (!basicHelper.isBTSymbolValid(oThis.symbol)) {
-        let errObj = responseHelper.error({
-          internal_error_identifier: 's_ob_pbt_2',
-          api_error_identifier: 'invalid_branded_token_symbol',
-          error_config: basicHelper.fetchErrorConfig()
-        });
-
-        return Promise.resolve(errObj);
-      }
-      if (!basicHelper.isBTConversionFactorValid(oThis.conversionFactor)) {
-        let errObj = responseHelper.error({
-          internal_error_identifier: 's_ob_pbt_3',
-          api_error_identifier: 'invalid_conversion_factor',
-          error_config: basicHelper.fetchErrorConfig()
-        });
-
-        return Promise.resolve(errObj);
-      }
-
-      const conversionRateConversionResponse = basicHelper.convertConversionFactorToConversionRate(oThis.conversionFactor);
-
-      if (conversionRateConversionResponse.isSuccess()) {
-
-        oThis.conversionRate = conversionRateConversionResponse.data.conversionRate;
-        oThis.conversionRateDecimals = conversionRateConversionResponse.data.conversionRateDecimals;
-
-        let OpenStUtilityContractInteractKlass = oThis.ic().getOpenSTUtilityeInteractClass()
-          , openSTUtilityContractInteract = new OpenStUtilityContractInteractKlass(oThis.openSTUtilityContractAddress)
-        ;
-
-        const proposalRsp = await openSTUtilityContractInteract.proposeBrandedToken(
-          oThis.stakerAddr,
-          oThis.stakerPassphrase, oThis.symbol,
-          oThis.name, oThis.conversionRate, oThis.conversionRateDecimals
-        );
-
-        if (proposalRsp.isSuccess()) {
-          proposalRsp.data.transaction_uuid = uuid.v4();
-        }
-
-        return Promise.resolve(proposalRsp);
-
-      } else {
-        return Promise.resolve(conversionRateConversionResponse);
-      }
-
-    } catch (err) {
+    //validations
+    if (!basicHelper.isBTNameValid(oThis.name)) {
       let errObj = responseHelper.error({
-        internal_error_identifier: 's_ob_pbt_4',
-        api_error_identifier: 'something_went_wrong',
-        error_config: basicHelper.fetchErrorConfig(),
-        debug_options: {err: err}
+        internal_error_identifier: 's_ob_pbt_1',
+        api_error_identifier: 'invalid_branded_token_name',
+        error_config: basicHelper.fetchErrorConfig()
       });
 
       return Promise.resolve(errObj);
     }
+    if (!basicHelper.isBTSymbolValid(oThis.symbol)) {
+      let errObj = responseHelper.error({
+        internal_error_identifier: 's_ob_pbt_2',
+        api_error_identifier: 'invalid_branded_token_symbol',
+        error_config: basicHelper.fetchErrorConfig()
+      });
+
+      return Promise.resolve(errObj);
+    }
+    if (!basicHelper.isBTConversionFactorValid(oThis.conversionFactor)) {
+      let errObj = responseHelper.error({
+        internal_error_identifier: 's_ob_pbt_3',
+        api_error_identifier: 'invalid_conversion_factor',
+        error_config: basicHelper.fetchErrorConfig()
+      });
+
+      return Promise.resolve(errObj);
+    }
+
+    const conversionRateConversionResponse = basicHelper.convertConversionFactorToConversionRate(oThis.conversionFactor);
+    if (conversionRateConversionResponse.isSuccess()) {
+
+      oThis.conversionRate = conversionRateConversionResponse.data.conversionRate;
+      oThis.conversionRateDecimals = conversionRateConversionResponse.data.conversionRateDecimals;
+
+      let OpenStUtilityContractInteractKlass = oThis.ic().getOpenSTUtilityeInteractClass()
+        , openSTUtilityContractInteract = new OpenStUtilityContractInteractKlass(oThis.openSTUtilityContractAddress)
+      ;
+
+      const proposalRsp = await openSTUtilityContractInteract.proposeBrandedToken(
+        oThis.stakerAddr,
+        oThis.stakerPassphrase, oThis.symbol,
+        oThis.name, oThis.conversionRate, oThis.conversionRateDecimals
+      );
+
+      if (proposalRsp.isSuccess()) {
+        proposalRsp.data.transaction_uuid = uuid.v4();
+      }
+
+      return Promise.resolve(proposalRsp);
+
+    } else {
+      return Promise.resolve(conversionRateConversionResponse);
+    }
+
   }
 };
 
